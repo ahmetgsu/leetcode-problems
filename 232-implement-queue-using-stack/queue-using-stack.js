998050716c94f899bbf5ef90679c83dabffe6ed8
Ahmet Kenar <a.kenar@shellrecharge.com>
2024-12-27 13:11:04 +0100

    Implement Queue using stacks

---
+++ 232-implement-queue-using-stack/queue-using-stack.js
@@ -0,0 +1,59 @@
+/*
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+
+*/
+
+class MyQueue {
+    constructor() {
+        this.stack = []
+    }
+    peek() {
+        return this.stack[0]
+    }
+
+    push(value) {
+        this.stack.push(value)
+        return this
+    }
+
+    pop() {
+        if (this.stack.length === 0) {
+            return null
+        }
+        const removedItem = this.stack.shift()
+        console.log("removedItem", removedItem)
+        return removedItem
+    }
+
+    empty() {
+        return this.stack.length === 0
+    }
+
+};
+var obj = new MyQueue()
+console.log(obj.push(2))
+console.log(obj.push(3))
+console.log(obj.pop())
+console.log(obj.empty())
+console.log(obj.pop())
+console.log(obj.empty())
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
