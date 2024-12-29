c7d5149a04aefa202c550268b0cd8bf6bc4321d7
Ahmet Kenar <a.kenar@shellrecharge.com>
2024-12-29 22:43:45 +0100

    Improve space complexity of initial solution

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -57,3 +57,22 @@ var hasCycle = function(head) {
     }
     return false
 };
+
+var hasCycle2 = function(head) {
+    if (!head || !head.next) {
+        return false;
+    }
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            return true;
+        }
+    }
+
+    return false;
+};
